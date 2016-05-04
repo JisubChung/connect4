f557df76f6007a4427e8bd653e4fe33520867658
JisubChung <jisub.chung0@gmail.com>
2016-05-03 23:11:16 -0700

    some housekeeping

--- app.js
+++ app.js
@@ -2,6 +2,16 @@
  * Created by jisub on 5/3/16.
  */
 
+// Module dependancies
 var express = require('express');
 var app = express();
 var path = require('path');
+var dotenv = require('dotenv');
+
+// Load environment variables.
+dotenv.load({ path: '.env' });
+
+
+// server port
+app.listen(3000);
+console.log('server is running');
