6577d8053638ad3c451deafc4f34a30c28d4562c
JisubChung <jisub.chung0@gmail.com>
2016-05-04 11:25:47 -0700

    Decided not to use ejs for now

--- app.js
+++ app.js
@@ -11,7 +11,21 @@ var dotenv = require('dotenv');
 // Load environment variables.
 dotenv.load({ path: '.env' });
 
+/**
+ * Express server settings
+ * */
+// setting up views folder for views (using ejs)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-// server port
+// express to use static assets
+app.use(express.static('public'));
+
+// set routes
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+// Start the server!
 app.listen(3000);
 console.log('server is running');
